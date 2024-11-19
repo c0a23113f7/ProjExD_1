c7863a924b571fc186e8c2f0c1c44dd945e1a626
Noge <c0a23113f7@edu.teu.ac.jp>
2024-11-19 16:30:30 +0900

    練習8:矢印キーの移動設定

--- pygame_image.py
+++ pygame_image.py
@@ -13,16 +13,29 @@ def main():
     bg_img2 = pg.transform.flip(bg_img, True, False)
     koukaton = pg.image.load("fig/3.png")
     koukaton = pg.transform.flip(koukaton, True, False)
+    kouka_rect = koukaton.get_rect() 
+    kouka_rect.center = 300, 200
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
+        key_lst = pg.key.get_pressed()
+        if key_lst[pg.K_UP]:
+            kouka_rect.move_ip((0, -1))
+        if key_lst[pg.K_DOWN]:
+            kouka_rect.move_ip((0, 1))
+        if key_lst[pg.K_RIGHT]:
+            kouka_rect.move_ip((1, 0))
+        if key_lst[pg.K_LEFT]:
+            kouka_rect.move_ip((-1, 0))
+
+
         x = -(tmr%3200)
         screen.blit(bg_img, [x, 0])
         screen.blit(bg_img2, [x+1600, 0])
         screen.blit(bg_img, [x+3200, 0])
         screen.blit(bg_img2, [x+4800, 0])
-        screen.blit(koukaton,[300, 200])
+        screen.blit(koukaton,kouka_rect)
         pg.display.update()
         tmr += 1        
         clock.tick(200)
