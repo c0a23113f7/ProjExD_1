9e8776f446198e364ed1fae1f332e30f099094c0
Noge <c0a23113f7@edu.teu.ac.jp>
2024-11-19 15:46:39 +0900

    練習4:工科トンの位置設定

--- pygame_image.py
+++ pygame_image.py
@@ -10,7 +10,7 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
-    koukaton = pg.image.load("3.png")
+    koukaton = pg.image.load("fig/3.png")
     koukaton = pg.transform.flip(koukaton, True, False)
     tmr = 0
     while True:
@@ -18,6 +18,7 @@ def main():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [0, 0])
+        screen.blit(koukaton,[300, 200])
         pg.display.update()
         tmr += 1        
         clock.tick(10)
